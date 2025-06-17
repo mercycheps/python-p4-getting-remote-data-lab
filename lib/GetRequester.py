a2b7fd9289656e954c5e1258ad6e11549523fed0
Mercy <maritimmercy8@gmail.com>
2025-06-17 08:04:34 +0300

    added json and response

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,11 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        return requests.get(self.url).content 
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
